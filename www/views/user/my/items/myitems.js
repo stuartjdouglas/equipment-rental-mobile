7e9c6a036c257cd7d96d0b8c6fda35403d7ff1fd
remony <sudouglas@dundee.ac.uk>
2016-01-08 21:19:26 +0000

    You can now see the items you currently hold

--- www/views/user/my/items/myitems.js
+++ www/views/user/my/items/myitems.js
@@ -1,38 +1,60 @@
 angular.module('App.my.items', [])
   .controller('myItemsCtrl', function($scope, $http, $rootScope) {
 
+    $scope.count = 15;
+
+
+    $scope.start = 0;
+
+
+
     if ($rootScope.loggedIn) {
+      updateResults();
+    } else {
+      $scope.view = false;
+    }
+
+    $scope.$watch("count", function(newValue) {
+      // console.log(newValue);
+      window.localStorage.setItem("product_count", newValue);
+      updateResults();
+    });
+
+    $scope.back = function() {
+      if ($scope.start - $scope.count < 0) {
+        $scope.viewResults = false;
+      } else {
+        $scope.viewResults = true;
+        $scope.start = $scope.start - $scope.count;
+        updateResults();
+      }
+    }
+
+    $scope.forward = function() {
+      if ($scope.start >= $scope.products.total - $scope.count) {
+        $scope.viewResults = false;
+      } else {
+        $scope.viewResults = true;
+        $scope.start = $scope.start + $scope.count;
+        updateResults();
+      }
+    }
+
+    function updateResults() {
       $http({
         url: backend + "/p/rent/current",
         method: 'GET',
         headers: {
-          'Content-Type': 'multipart/form-data',
-          'token': window.localStorage.token,
-          'step':0,
-          'count':5
+          'Start':$scope.start,
+          'Count':$scope.count,
+          'token': window.localStorage.token
         }
       }).success(function(data, status, headers, config) {
-        $scope.profile = data.profile;
-        $scope.view = true;
-        $http({
-          url: backend + "/identify/qr/user",
-          method: 'GET',
-          headers: {
-            'token': window.localStorage.token,
-            'width': 300,
-            'height': 300,
-            'code': data.profile.username
-          }
-        }).success(function(data, status, headers, config) {
-
-          $scope.qr = data;
-        }).
-          error(function(data, status, headers, config) {
-            $scope.error = true;
-          });
-      })
-    } else {
-      console.log("you need to be loggged in");
+        $scope.products = data;
+      }).
+      error(function(data, status, headers, config) {
+        $scope.error = true;
+      });
     }
 
   });
